33a9767484b4d9a4819695f4cbe79d79098fc5da
Mstrodl <ipadlover8322@gmail.com>
2017-05-10 15:58:36 -0400

    No need to specify API endpoint on the most desirable config

--- web/logins.js
+++ web/logins.js
@@ -1,4 +1,4 @@
-let APIBase = "http://0.0.0.0:8000"
+let APIBase = "" // "http://0.0.0.0:8000"
 
 $(document).ready(function() {
     document.getElementById("login").onclick = login
@@ -6,14 +6,14 @@ $(document).ready(function() {
 
 function login() {
     console.log("Henlo")
-    $.post(APIBase + "/api/auth/login", { email: document.getElementById("email").value, password: document.getElementById("password").value }, function (result) {
+    $.post(APIBase + "/api/auth/login", JSON.stringify({ email: document.getElementById("email").value, password: document.getElementById("password").value }), function (result) {
         window.localStorage.setItem("token", result.token)
         window.location.href = "serverview.html"
     });
 }
 
 function signup() {
-    $.post(APIBase + "/api/users/add", { email: document.getElementById("email"), username: document.getElementById("username"), password: document.getElementById("password") }, function (result) {
+    $.post(APIBase + "/api/users/add", JSON.stringify({ email: document.getElementById("email"), username: document.getElementById("username"), password: document.getElementById("password") }), function (result) {
         if (result.code != 1 || result.message != "success") return alert(new Error("An error occurred while signing you up :( Error " + result.code + ": " + result.message))
         login()
     });
