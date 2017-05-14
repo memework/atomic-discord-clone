d309896ec51c6a7b94751565ce30c5a9322c09f1
Mstrodl <ipadlover8322@gmail.com>
2017-05-14 12:26:03 -0400

    Oops login page is kill

--- web/logins.js
+++ web/logins.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
 function login() {
     console.log("Henlo")
     $.post(APIBase + "/api/auth/login", JSON.stringify({ email: document.getElementById("email").value, password: document.getElementById("password").value }), function (result) {
-        if(!(result instanceof Object)) result = JSON.parse(result)
+        if(typeof result == "String") result = JSON.parse(result)
         window.localStorage.setItem("token", result.token)
         window.location.href = "serverview.html"
     });
@@ -30,7 +30,7 @@ function login() {
 
 function signup() {
     $.post(APIBase + "/api/users/add", JSON.stringify({ email: document.getElementById("email").value, username: document.getElementById("username").value, password: document.getElementById("password").value}), function (result) {
-        if(!(result instanceof Object)) result = JSON.parse(result)
+        if(typeof result == "String") result = JSON.parse(result)
         if (result.code != 1 || result.message != "success") return alert(new Error("An error occurred while signing you up :( Error " + result.code + ": " + result.message))
         login()
     });
