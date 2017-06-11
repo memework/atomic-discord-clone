87e89985a29f3c94a1ef1ae2008b08eb4f7492f2
Mstrodl <ipadlover8322@gmail.com>
2017-06-11 09:56:13 -0400

    /api/users/add -> /api/auth/users/add

--- web/logins.js
+++ web/logins.js
@@ -52,7 +52,7 @@ function login() {
 }
 
 function signup() {
-  $.post(APIBase + "/api/users/add", JSON.stringify({ email: document.getElementById("email").value, username: document.getElementById("username").value, password: document.getElementById("password").value }), function (result) {
+  $.post(APIBase + "/api/auth/users/add", JSON.stringify({ email: document.getElementById("email").value, username: document.getElementById("username").value, password: document.getElementById("password").value }), function (result) {
     if (typeof result == String) result = JSON.parse(result)
     if (result.code != 1 || result.message != "success") return alert("An error occurred while signing you up :( Error " + result.code + ": " + result.message)
     login()
